19288bb2c99d79b6b8103006520ab5f3c55af9f7
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-06-28 20:47:52 -0500

    Passing Dynamic Data in HTML - Part 2 V2

--- envLab06/src/projects/lab06/travello/views.py
+++ envLab06/src/projects/lab06/travello/views.py
@@ -3,7 +3,20 @@ from .models import Destination
 # Create your views here.
 def index(request):
     dest1 = Destination()
-    dest1.name = 'prueba1'
-    dest1.desc = 'Esta ciudad sera una ciudad de peru'
-    dest1.price = 700
-    return render(request,'index.html', {'dest1': dest1})
+    dest1.name = 'Cuzco'
+    dest1.desc = 'Donde la historia se entrelaza con la grandeza de los Andes.'
+    dest1.price = 1500
+    dest1.img = 'destination1.jpg'
+    dest2 = Destination()
+    dest2.name = 'Chivay'
+    dest2.desc = 'Donde la serenidad se encuentra en los paisajes de los cañones profundos.'
+    dest2.price = 1200
+    dest2.img = 'destination2.jpg'
+    dest3 = Destination()
+    dest3.name = 'Puno'
+    dest3.desc = 'Donde el misterio del lago Titicaca se despliega en todo su esplendor'
+    dest3.price = 800
+    dest3.img = 'destination3.jpg'
+
+    dests = [dest1,dest2,dest3]
+    return render(request,'index.html', {'dests': dests})
