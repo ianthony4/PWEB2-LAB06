92c09c7633ec3c2c080fe8e5a75210737ac046e9
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-06-28 21:18:02 -0500

    IF statement in Django - Imagenes extra

--- envLab06/src/projects/lab06/travello/models.py
+++ envLab06/src/projects/lab06/travello/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Destination:
-    id: int
-    name: str
-    img: str
-    desc: str
-    price: int
+    idCiudad: int
+    nombreCiudad: str
+    imagenCiudad: str
+    descipcionCiudad: str
+    priceTour: int
+    ofertaTour : bool
--- envLab06/src/projects/lab06/travello/views.py
+++ envLab06/src/projects/lab06/travello/views.py
@@ -3,20 +3,23 @@ from .models import Destination
 # Create your views here.
 def index(request):
     dest1 = Destination()
-    dest1.name = 'Cuzco'
-    dest1.desc = 'Donde la historia se entrelaza con la grandeza de los Andes.'
-    dest1.price = 1500
-    dest1.img = 'destination1.jpg'
+    dest1.nombreCiudad = 'Cuzco'
+    dest1.descipcionCiudad = 'Donde la historia se entrelaza con la grandeza de los Andes.'
+    dest1.priceTour = 1500
+    dest1.imagenCiudad = 'destination1.jpg'
+    dest1.ofertaTour = True
     dest2 = Destination()
-    dest2.name = 'Chivay'
-    dest2.desc = 'Donde la serenidad se encuentra en los paisajes de los cañones profundos.'
-    dest2.price = 1200
-    dest2.img = 'destination2.jpg'
+    dest2.nombreCiudad = 'Chivay'
+    dest2.descipcionCiudad = 'Donde la serenidad se encuentra en los paisajes de los cañones profundos.'
+    dest2.priceTour = 1200
+    dest2.imagenCiudad = 'destination2.jpg'
+    dest2.ofertaTour = False
     dest3 = Destination()
-    dest3.name = 'Puno'
-    dest3.desc = 'Donde el misterio del lago Titicaca se despliega en todo su esplendor'
-    dest3.price = 800
-    dest3.img = 'destination3.jpg'
+    dest3.nombreCiudad = 'Puno'
+    dest3.descipcionCiudad = 'Donde el misterio del lago Titicaca se despliega en todo su esplendor'
+    dest3.priceTour = 800
+    dest3.imagenCiudad = 'destination3.jpg'
+    dest3.ofertaTour = False
 
     dests = [dest1,dest2,dest3]
     return render(request,'index.html', {'dests': dests})
