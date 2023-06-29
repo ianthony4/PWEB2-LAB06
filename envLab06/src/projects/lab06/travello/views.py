e3975157ee82dae46fbd8f8011b067b80ec56996
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-06-28 20:15:48 -0500

    Passing Dynamic Data in HTML - Part 2

--- envLab06/src/projects/lab06/travello/views.py
+++ envLab06/src/projects/lab06/travello/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    dest1 = Destination()
+    dest1.name = 'prueba1'
+    dest1.desc = 'Esta ciudad sera una ciudad de peru'
+    dest1.price = 700
+    return render(request,'index.html', {'dest1': dest1})
